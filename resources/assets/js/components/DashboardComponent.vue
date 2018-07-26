7093c5beff19cee6a1dca72bfa9cdd81814a747f
Dark Pikachu <Dark_Knightt@hotmail.com>
2018-07-26 16:28:08 +0700

    add calendar

--- resources/assets/js/components/DashboardComponent.vue
+++ resources/assets/js/components/DashboardComponent.vue
@@ -1,13 +1,36 @@
 <template>
-    <b-container></b-container>
+    <b-container>
+        <full-calendar :events="events"></full-calendar>
+    </b-container>
 </template>
 
 <script>
-    export default {
+    import { FullCalendar } from 'vue-full-calendar'
 
+    export default {
+        data() {
+            return {
+            events: [
+                {
+                    title  : 'event1',
+                    start  : '2010-01-01',
+                },
+                {
+                    title  : 'event2',
+                    start  : '2010-01-05',
+                    end    : '2010-01-07',
+                },
+                {
+                    title  : 'event3',
+                    start  : '2010-01-09T12:30:00',
+                    allDay : false,
+                },
+            ]
+            }
+        }
     }
 </script>
 
 <style>
-
-</style>
+    @import '~fullcalendar/dist/fullcalendar.css';
+</style> 
